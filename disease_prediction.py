0fed54aea879a3f2df13f674a9cd9c657d160be4
unknown <hetarth002@gmail.com>
2023-10-04 20:27:23 +0530

    complete model

--- disease_prediction.py
+++ disease_prediction.py
@@ -5,16 +5,21 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from sklearn.metrics import accuracy_score, confusion_matrix
+from scipy.stats import mode
 
-training = pd.read_csv('Data/Training.csv').dropna(axis=1)
-testing = pd.read_csv('Data/Testing.csv').dropna(axis=1)
-
-x = training.iloc[:, :-1].values
-y = training.iloc[:, -1].values
+dataset = pd.read_csv('Data/Training.csv').dropna(axis=1)
+encoder = LabelEncoder()
+dataset["prognosis"] = encoder.fit_transform(dataset["prognosis"])
+x = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.3, random_state=12)
+    x, y, test_size=0.2, random_state=24)
+
 
 lr = LogisticRegression()
 lr.fit(x_train, y_train)
@@ -58,3 +63,60 @@ rf.fit(x_train, y_train)
 pred7 = rf.predict(x_test)
 accuracy7 = accuracy_score(y_test, pred7)*100
 accuracy7
+
+final_svm_model = SVC()
+final_nb_model = GaussianNB()
+final_rf_model = RandomForestClassifier(random_state=18)
+final_svm_model.fit(x, y)
+final_nb_model.fit(x, y)
+final_rf_model.fit(x, y)
+
+test_data = pd.read_csv("Data/Testing.csv").dropna(axis=1)
+
+test_X = test_data.iloc[:, :-1]
+test_Y = encoder.transform(test_data.iloc[:, -1])
+
+svm_preds = final_svm_model.predict(test_X)
+nb_preds = final_nb_model.predict(test_X)
+rf_preds = final_rf_model.predict(test_X)
+
+symptoms = x.columns.values
+
+symptom_index = {}
+for index, value in enumerate(symptoms):
+    symptom = " ".join([i.capitalize() for i in value.split("_")])
+    symptom_index[symptom] = index
+
+data_dict = {
+    "symptom_index": symptom_index,
+    "predictions_classes": encoder.classes_
+}
+
+
+def predictDisease(symptoms):
+    symptoms = symptoms.split(",")
+    input_data = [0] * len(data_dict["symptom_index"])
+    for symptom in symptoms:
+        index = data_dict["symptom_index"][symptom]
+        input_data[index] = 1
+    input_data = np.array(input_data).reshape(1, -1)
+
+    rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[
+        0]]
+    nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[
+        0]]
+    svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[
+        0]]
+    final_prediction = mode(
+        [rf_prediction, nb_prediction, svm_prediction])[0][0]
+    predictions = {
+        "rf_model_prediction": rf_prediction,
+        "naive_bayes_prediction": nb_prediction,
+        "svm_model_prediction": svm_prediction,
+        "final_prediction": final_prediction
+    }
+    return predictions
+
+
+print(predictDisease(
+    "Slurred Speech,Cramps,Nodal Skin Eruptions,Dizziness,Loss Of Balance"))
